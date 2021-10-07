8aed36d4e01c8fd3e8c1d1fc405271dcd90f6e3e
Emmeleia Nix <emmeleianix@campus-113-006.ucdavis.edu>
2021-10-07 14:16:38 -0700

    test2

---
+++ scripts/week2_notes.R
@@ -0,0 +1,24 @@
+a<-2
+a+a
+letters
+b<-3
+a+b
+(number_vector = c(3,44,56))
+number_vector[2]
+number_vector[3:1]
+number_vector = c(number_vector,78)
+number_vector
+"string"
+(my_string <- c('dog','walrus','cat'))
+abc <- data.frame (first_column = my_string, second_column = my_string,)
+
+my_string
+?str()
+class(my_string)
+?list()
+test_list = list('help me learn R','ahhh',a)           
+test_list
+str(test_list)
+number_vector <- c(abc,78,96)
+number_vector
+""
---
+++ scripts/week3_notes.R
@@ -0,0 +1,3 @@
+surveys <- read.csv("data/portal_data_joined.csv")
+head(surveys)
+View(surveys)
