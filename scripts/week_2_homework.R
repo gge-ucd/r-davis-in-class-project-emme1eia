3be6de104ff3a8d5a5afb4a88cc41ba73cc35cf2
Emmeleia Nix <emmeleianix@campus-113-006.ucdavis.edu>
2021-10-07 13:01:32 -0700

    Completed hw

---
+++ scripts/week_2_homework.R
@@ -0,0 +1,10 @@
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+prob1 <- hw2[!is.na(hw2)]
+prob1 <- prob1[prob1>14 & prob1<38]
+prob1
+times3 <- prob1*3
+times3
+plus10 <- times3+10
+plus10[c(1,3,5,7,9,11,13,15,17,19,21,23)]
