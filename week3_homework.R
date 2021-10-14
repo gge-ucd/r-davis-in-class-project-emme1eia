587aa7536242320a6a1a5816954c71c29d7aaa73
Emmeleia Nix <emmeleianix@campus-020-120.ucdavis.edu>
2021-10-14 14:48:06 -0700

    hw

--- week3_homework.R
+++ week3_homework.R
@@ -1,10 +1,9 @@
 surveys <- read.csv("data/portal_data_joined.csv")
 head(surveys)
-surveys_base <- surveys[1:60, c(6,9,13)]
-head(surveys_base)
-surveys_base$species_id <- as.character(surveys_base$species_id)
+surveys_base <- surveys[c('species_id','weight','plot_type')]
+surveys_base <- surveys_base[1:60,]
+surveys_base$species_id <- as.factor(surveys_base$species_id)
 str(surveys_base)
-surveys_base$plot_type <- as.character(surveys_base$plot_type)
-surveys_base$weight <- surveys_base$weight(x, na.rm=TRUE)
-surveys_base <- surveys_base[complete.cases(surveys_base),]
+surveys_base$plot_type <- as.factor(surveys_base$plot_type)
+surveys_base$weight <- na.omit(surveys_base$weight)
 head(surveys_base)
